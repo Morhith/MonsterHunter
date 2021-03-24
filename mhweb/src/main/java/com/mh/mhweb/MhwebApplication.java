54b5b7f746b75c1eb1103baea0f5bdaff554de50
‘morhith’ <yehith@sina.com>
2021-03-24 23:38:20 +0800

    service框架

---
+++ mhweb/src/main/java/com/mh/mhweb/MhwebApplication.java
@@ -0,0 +1,17 @@
+package com.mh.mhweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+//自动填充或更新实体中的CreateDate、CreatedBy
+@EnableJpaAuditing
+public class MhwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhwebApplication.class, args);
+		System.out.println("springboot 启动成功！");
+	}
+
+}
---
+++ mhweb/src/test/java/com/mh/mhweb/MhwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mh.mhweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
